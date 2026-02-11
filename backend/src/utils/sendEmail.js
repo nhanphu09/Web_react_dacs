05a02b10c5f68042961b86b7df39af3ebd7c166e
Nhân Phú <nhanphu@MacBook-Pro-cua-Nhan.local>
2026-02-11 19:33:13 +0700

    update smtp port to 587 for render

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -6,8 +6,8 @@ dotenv.config();
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: "smtp.gmail.com",
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false, // true for 465, false for other ports
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
