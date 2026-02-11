d1618d98e67a76d0bc95b7bd61386f519f8707a9
Nhân Phú <nhanphu@MacBook-Pro-cua-Nhan.local>
2026-02-11 20:33:55 +0700

    use service gmail fallback

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -5,9 +5,7 @@ dotenv.config();
 
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        service: "gmail",
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
