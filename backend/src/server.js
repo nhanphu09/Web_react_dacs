9e5d1221e13a44ab65e499c9675f2d6857ed24d5
Nhân Phú <nhanphu@192.168.1.3>
2026-02-23 20:37:23 +0700

    Fix: Increase API rate limit to fix 429 error

--- backend/src/server.js
+++ backend/src/server.js
@@ -49,7 +49,7 @@ app.use(express.json());
 // Rate Limiting
 const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // limit each IP to 100 requests per windowMs
+	max: 500, // limit each IP to 100 requests per windowMs
 	message: "Too many requests from this IP, please try again after 15 minutes",
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
